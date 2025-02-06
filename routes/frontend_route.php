b34398364a77f02436854e3fa292180eeb3666c1
Lan0410 <lcu14623@gmail.com>
2025-02-06 08:43:47 +0700

    haidv upcode

--- routes/frontend_route.php
+++ routes/frontend_route.php
@@ -26,11 +26,21 @@ Route::get('/invitation/{id}',function(Request $request,$id){
         $data['event_address'] = '156 Ngô Quyền - TP Hải Dương - Hải Dương';
     }
     else{
-        $data['event_name'] = 'LỄ CƯỚI NHÀ NỮ';
-        $data['event_time'] = '17:00 PM';
-        $data['event_date'] = 'Thứ bảy, NGÀY 15/02/2025';
-        $data['event_date_dot'] = '16.02.2025';
-        $data['event_address'] = 'Thôn Kim Đôi - Xã Cẩm Hoàng - Cẩm Giàng - HD';
+        if($user->type == 3) {
+            $data['event_name'] = 'TIỆC CƯỚI NHÀ NAM';
+            $data['event_time'] = '17:00 PM';
+            $data['event_date'] = 'Thứ bảy, NGÀY 15/02/2025';
+            $data['event_date_dot'] = '16.02.2025';
+            $data['event_address'] = '156 Ngô Quyền - TP Hải Dương - Hải Dương';
+        }
+        else {
+
+            $data['event_name'] = 'LỄ CƯỚI NHÀ NỮ';
+            $data['event_time'] = '17:00 PM';
+            $data['event_date'] = 'Thứ bảy, NGÀY 15/02/2025';
+            $data['event_date_dot'] = '16.02.2025';
+            $data['event_address'] = 'Thôn Kim Đôi - Xã Cẩm Hoàng - Cẩm Giàng - HD';
+        }
     }
     return view('thiepmoi',compact('data'));
 });
